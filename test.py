c9aa6cf2f2d4ad62338e07adbbcfd0a1584539de
ZapPRO <61541711+ZapPRO1256@users.noreply.github.com>
2025-05-27 15:10:13 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import os
 print("Вміст папки:", os.listdir("."))
-print("Розмір keyword_article_model.keras:", os.path.getsize("keyword_article_model.keras"))
+print("Розмір keyword_article_model.keras:", os.path.getsize("keyword_article_model.h5"))
 
 from keras.models import load_model
-model = load_model("keyword_article_model.keras")
+model = load_model("keyword_article_model.h5")
